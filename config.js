f04e963d8c7dfa610ab2eabde42f5ec1e5eb33c4
M250R <155747446+M250R@users.noreply.github.com>
2024-04-02 02:05:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,22 +2,22 @@
 
 module.exports = {
     token: '', // التوكن
-    serverId: '', // ايدي السيرفر
-    textChannelId: '', // ايدي الروم
+    serverId: '887645332824031233', // ايدي السيرفر
+    textChannelId: '1224494787814756504', // ايدي الروم
     language: 'en', // ar | en
-    updateInterval: 20000, // وقت تحديث الامبد
+    updateInterval: 10000, // وقت تحديث الامبد
     commandPrefix: '!status', // الامر مع البرفكس
     showAllMembers: true, // جميع الاعضاء
     showBotCount: true, // جميع البوتات
     showTextChannels: true, // رومات الكتابية
     showVoiceChannels: true, // رومات الصوتية
-    showRoles: true, // الرتب
+    showRoles: false, // الرتب
     showBoostCount: true, // البوستات
     showServerId: true, // اخفاء / اظهار ايدي السيرفر
     showServerCreationDate: true, // تاريخ انشاء السيرفر
     showServerOwner: true, // اخفاء / اظهار سيرفر اونر
     Color: '#07132b', // لون الامبد
-    ownerId: '', // ايدي سيرفر اونر
+    ownerId: '678215084014829618', // ايدي سيرفر اونر
     roleIdToTrack: '', // ايدي رتبة الزبون
-    Banner: '', // صورة البنر
+    Banner: 'https://cdn.discordapp.com/icons/887645332824031233/b385932022d368d04abc9d593cc64ea3.png?size=4096', // صورة البنر
 };
